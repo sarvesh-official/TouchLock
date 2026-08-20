02e5211b29b1cac39e6eecbe6bc952ebb6e0ec29
Sarvesh P <sarveshjaiofficial@gmail.com>
2026-08-21 02:07:07 +0530

    Add fragment-ktx dependency to fix lint vital release build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -103,6 +103,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.material3:material3")
